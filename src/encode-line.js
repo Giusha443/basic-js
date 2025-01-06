09f6790fac862bd888090b85fd1fdfcf457f9b98
Giusha443 <giorgi.jajanidze2003@gmail.com>
2025-01-06 22:53:06 +0400

    feat: add more

--- src/encode-line.js
+++ src/encode-line.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given a string, return its encoding version.
@@ -10,11 +10,29 @@ const { NotImplementedError } = require('../extensions/index.js');
  * For aabbbc should return 2a3bc
  *
  */
-function encodeLine(/* str */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function encodeLine(str) {
+  if (!str) return "";
+
+  let result = "";
+  let count = 1;
+  let currentChar = str[0];
+
+  for (let i = 1; i <= str.length; i++) {
+    if (i < str.length && str[i] === currentChar) {
+      count++;
+    } else {
+      if (count > 1) {
+        result += count;
+      }
+      result += currentChar;
+      count = 1;
+      currentChar = str[i];
+    }
+  }
+
+  return result; // remove line with error and write your code here
 }
 
 module.exports = {
-  encodeLine
+  encodeLine,
 };
--- src/matrix-elements-sum.js
+++ src/matrix-elements-sum.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js");
 
 /**
  * Given matrix, a rectangular matrix of integers,
@@ -16,11 +16,14 @@ const { NotImplementedError } = require('../extensions/index.js');
  *
  * The result should be 9
  */
-function getMatrixElementsSum(/* matrix */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+function getMatrixElementsSum(matrix) {
+  throw new NotImplementedError("Not implemented");
+  // return matrix
+  //   .flatMap((x) => x)
+  //   .filter((x) => x >= 0)
+  //   .reduce((acc, i) => acc + Number(i), 0); // remove line with error and write your code here
 }
 
 module.exports = {
-  getMatrixElementsSum
+  getMatrixElementsSum,
 };
